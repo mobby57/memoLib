1799cc61bc01241a0c6470ef104001ed83bb6255
mooby865 <morosidibepro@gmail.com>
2025-12-19 23:50:12 +0100

    fix: Add missing WebDriverException import to E2E tests

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import WebDriverException
 import time
 
 def is_server_running(url="http://localhost:5000"):
